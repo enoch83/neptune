41f0da6bfb5059994e5dc2aab9d90e84f951e5a3
Jesper Malmström <jesper.malmstrom@enoch83.com>
2017-09-17 12:33:30 +0100

    ReadCsv
    Add Helper for reading csv file

---
+++ src/Neptune/Neptune.ReadCsvExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Neptune.Helpers;
+using System;
+
+namespace Neptune.ReadCsvExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "ALFA.ST.csv";
+
+            // To import a DataFrame from a csv file, use the function ReadCsv.
+            // The only thing we need to pass to the function is a file path.
+            DataFrame df = ReadFile.ReadCsv(filePath);
+            Console.WriteLine(df);
+
+            // It's possible to get A DataFrame with only some columns using the indexer with a column index. If we want the 'Open' column, pass 1 to the indexer
+            // We can also get specific rows using the ILoc function. To select the second row, pass 1
+            // Remeber, both the indexer and ILoc is zero-based
+            var open = df[1];
+            Console.WriteLine(open);
+
+            var secondRow = df.ILoc(1);
+            Console.WriteLine(secondRow);
+
+            // If we want to use any of the rows in the file as headers, pass the param 
+            DataFrame df2 = ReadFile.ReadCsv(filePath, headerRow: 0);
+            Console.WriteLine(df2);
+
+            // We just added the ability to select columns by headers.
+            // This is not possible without the headers, it would throw IndexOutOfRangeException
+            DataFrame open2 = df2["Open"];
+            Console.WriteLine(open2);
+
+            // By passing 
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Neptune/Neptune/Helpers/ReadData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Neptune.Helpers
+{
+    public static class ReadFile
+    {
+        /// <summary>
+        /// Get a DataSet from a file
+        /// </summary>
+        /// <param name="filePath">File to read data from</param>
+        /// <param name="separator">char representing seperator used to define columns</param>
+        /// <param name="skipRows">Number of rows to skip when reading, start at 1</param>
+        /// <param name="headerRow">Index pointing at what row to use as header</param>
+        /// <param name="indexColumn">Index pointing at what column to use as indexers</param>
+        /// <returns>A DataFrame</returns>
+        public static DataFrame ReadCsv(string filePath, char separator = ';', int skipRows = 0, int? headerRow = null, int? indexColumn = null)
+        {
+            int numerOfRows = 0;
+            var AllLines = new string[10000]; //only allocate memory here
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (numerOfRows >= skipRows)
+                    {
+                        AllLines[numerOfRows - skipRows] = line;
+                    }
+
+                    numerOfRows += 1;
+                }
+            }
+
+            // Get the number of columns
+            int numberOfColumns = AllLines[headerRow != null ? (int)headerRow : 0].Split(separator).Length - (indexColumn != null ? 1 : 0);
+
+            string[] headers = null;
+            string[] indexes = indexColumn != null ? new string[numerOfRows - skipRows - 1] : null;
+
+            Series[] series = new Series[headerRow != null ? numerOfRows - 1 : numerOfRows];
+
+            for (int i = 0; i < numerOfRows - skipRows; i++)
+            {
+                var line = AllLines[i].Split(separator);
+
+                if (i == headerRow && headerRow != null)
+                {
+                    headers = line;
+
+                    if (indexColumn != null)
+                    {
+                        headers = headers.Where((source, index) => index != (int)indexColumn).ToArray();
+                    }
+                }
+                else
+                {
+                    object[] data = new object[numberOfColumns];
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        if (indexColumn != null && j == indexColumn)
+                        {
+                            indexes[headerRow != null ? i - 1 : i] = line[j];
+                        }
+                        else
+                        {
+                            data[j] = line[j];
+                        }
+                    }
+
+                    series[headerRow != null ? i - 1 : i] = new Series(data);
+                }
+            }
+
+            SeriesArray array = new SeriesArray(series);
+
+            return new DataFrame(array, headers, indexes);
+        }
+    }
+}
