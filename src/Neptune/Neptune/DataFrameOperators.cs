56fa363224b542352788b3693be45ebd0ba97f84
Jesper Malmström <jesper.malmstrom@enoch83.com>
2017-10-02 10:57:59 +0100

    DataFrameOperators test + cast
    Added the ability to cast a dataframe to double and double[] + added som test for it

--- src/Neptune/Neptune/DataFrameOperators.cs
+++ src/Neptune/Neptune/DataFrameOperators.cs
@@ -96,6 +96,30 @@ namespace Neptune
             return new DataFrame(seriesArray, headers, df1.Indexers);
         }
 
+        public static implicit operator double(DataFrame df)
+        {
+            if (df.Array.GetLength(0) > 1)
+                throw new ArgumentOutOfRangeException("Length of DataFrame must by 1, try double[].");
+
+            double value = 0;
+            double.TryParse(df.Array[0][0].ToString(), out value);
+
+            return value;
+        }
+
+        public static implicit operator double[](DataFrame df)
+        {
+            double[] values = new double[df.Array.GetLength(0)];
+            for (int i = 0; i < df.Array.GetLength(0); i++)
+            {
+                double value = 0;
+                double.TryParse(df.Array[i][0].ToString(), out value);
+
+                values[i] = value;
+            }
+
+            return values;
+        }
 
         private static void ValidateData(DataFrame df1, DataFrame df2)
         {
